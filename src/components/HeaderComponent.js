c9ce5b62c4204c826e9984bd80f7cc1c5faf2dea
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-06-15 19:15:54 -0700

    Erased All Content - Start over

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,99 +1,10 @@
-import { useState } from 'react';
-import {
-	Nav,
-	Navbar,
-	NavbarBrand,
-	NavbarToggler,
-	Collapse,
-	NavItem,
-	Button,
-	Modal,
-	ModalHeader,
-	ModalBody,
-	Form,
-	FormGroup,
-	Input,
-	Label
-} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
 
 const Header = () => {
-	const [ isNavOpen, setNavOpen ] = useState(false);
-	const [ isModalOpen, setModalOpen ] = useState(false);
-
-	const toggleNav = () => {
-		setNavOpen(!isNavOpen);
-	};
-
-	const toggleModal = () => {
-		setModalOpen(!isModalOpen);
-	};
-
-	const handleLogin = (event) => {
-		alert(`Username: ${event.username.value} Password: ${event.password.value} Remember: ${event.remember.checked}`);
-		toggleModal();
-		event.preventDefault();
-	};
+	
 
 	return (
-		<div id="header">
-			<Navbar className="headerbackground p-0 mx-auto " sticky="top" expand="md">
-				<div className="container">
-					<NavbarBrand className="mr-auto navtitle ml-2" href="/">
-						PRIVATE TIME
-					</NavbarBrand>
-					<NavbarToggler onClick={toggleNav} className="mr-2" />
-					<Collapse isOpen={isNavOpen} navbar>
-						<Nav navbar className="ml-auto text-center">
-							<NavItem>
-								<NavLink className="nav-link navstyle" to="/home">
-									HOME
-								</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink className="nav-link navstyle ml-md-3 ml-lg-5" to="/about">
-									ABOUT
-								</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink className="nav-link navstyle ml-md-3 ml-lg-5" to="/contactus">
-									CONTACT US
-								</NavLink>
-							</NavItem>
-						</Nav>
-						<span className="navbar-text mx-auto d-flex justify-content-center">
-							<Button outline onClick={toggleModal}>
-								LOGIN
-							</Button>
-						</span>
-					</Collapse>
-				</div>
-			</Navbar>
-			<Modal isOpen={isModalOpen} toggle={toggleModal}>
-				<ModalHeader toggle={toggleModal}>Login</ModalHeader>
-				<ModalBody>
-					<Form onSubmit={handleLogin}>
-						<FormGroup>
-							<Label htmlFor="username">Username</Label>
-							<Input type="text" id="username" name="username" innerRef={(input) => (input.username = input)} />
-						</FormGroup>
-						<FormGroup>
-							<Label htmlFor="password">Password</Label>
-							<Input type="password" id="password" name="password" innerRef={(input) => (input.password = input)} />
-						</FormGroup>
-						<FormGroup check>
-							<Label check>
-								<Input type="checkbox" name="remember" innerRef={(input) => (input.remember = input)} />
-								Remember me
-							</Label>
-						</FormGroup>
-						<Button type="submit" value="submit" color="primary">
-							Login
-						</Button>
-					</Form>
-				</ModalBody>
-			</Modal>
-		</div>
+		
 	);
 };
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,24 +1,9 @@
 import React, { Fragment } from 'react';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import Home from './HomeComponent';
-import About from './AboutComponent';
-import ContactUs from './ContactUsComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+
 
 const Main = () => {
 	return (
-		<Fragment>
-			<Header />
-			<Switch>
-				<Route path="/home" component={Home} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/contactus" component={ContactUs} />
-				<Redirect to="/home" />
-			</Switch>
-			<Footer />
-		</Fragment>
+		
 	);
 };
 
