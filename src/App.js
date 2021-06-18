a51f92a89586cb1667b8fb9b35cb3becddbb08b6
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-06-18 14:56:09 -0700

    Add logo and images file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Main from './components/MainComponent';
 import './App.css';
-import { Header } from './components/Header';
+import Header from './components/Header';
 
 const App = () => {
 	return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
 
 const Header = () => {
 	return (
-		
+		<Container>
+			<a href="#">
+				<img src="../public/images/nav-barBrand.png" width="80" />
+			</a>
+			<NavBar>
+
+			</NavBar>
+		</Container>
 	);
 };
 
 export default Header;
+
+const Container = styled.div`
+	display: flex;
+	min-height: 100px;
+	align-items: center;
+	justify-content: space-between;
+	background-image: linear-gradient(#aa273d, #e24a63, white);
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+
+`
+
+const NavBar = styled.div``
