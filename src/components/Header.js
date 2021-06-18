c074ebd88cae7aad8f92f80e727994cf52da5fa7
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-06-18 15:06:42 -0700

    Corrected Image file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,27 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-	return (
-		<Container>
-			<a href="#">
-				<img src="../public/images/nav-barBrand.png" width="80" />
+  return (
+    <Container>
+      <a href="#">
+        <img src="/images/nav-barBrand.png" width="80" />
 			</a>
-			<NavBar>
-
-			</NavBar>
-		</Container>
-	);
+			<h1 class="title">Private Time</h1>
+    </Container>
+  );
 };
 
 export default Header;
 
 const Container = styled.div`
-	display: flex;
-	min-height: 100px;
-	align-items: center;
-	justify-content: space-between;
-	background-image: linear-gradient(#aa273d, #e24a63, white);
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-
-`
-
-const NavBar = styled.div``
+  display: flex;
+  min-height: 80px;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(#aa273d, #e24a63, white);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
