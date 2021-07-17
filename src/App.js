0f0e5122cf62213d47bca8694d8a448661cd48b4
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-07-17 15:08:37 -0700

    returned app.js to original config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import GlobalStyle from "./globalStyles";
-// import { Navbar, Footer } from "./components";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <Router>
-      <GlobalStyle />
-      <ScrollToTop />
-      <Switch>
-        {/* <Route path="/" exact comonent={Home} /> */}
-        {/* <Route path='about' component={about} /> */}
-        {/* <Route path='contact' component={contact} /> */}
-      </Switch>
-      {/* <Footer /> */}
-    </Router>
+    <div className="App">
+      <Header />
+      <Navbar />
+    </div>
   );
 };
 
